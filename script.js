b7068bfaee16ba8affe5134028f5c323dc0c7add
Maximus Brutalis <dmitroid1sk@gmail.com>
2024-07-26 23:28:07 -0500

    Initial commit. Playable game

---
+++ script.js
@@ -0,0 +1,51 @@
+let playerScore = 0;
+let computerScore = 0;
+const items = ["rock", "scissors", "paper"];
+
+while (playerScore !== 3 && computerScore !==3) {
+    let computerChoiceIndex = Math.floor(Math.random() * items.length);
+    let computerChoice = items[computerChoiceIndex];
+    let playerChoice = prompt("Rock, Scissors or Paper?").toLowerCase()
+
+    if (playerChoice === "rock"){
+        if (computerChoice === "rock"){
+            console.log("Draw! Rock vs. Rock");
+        } else if (computerChoice === "paper"){
+            console.log("Sorry, you lose... Paper beats Rock");
+            computerScore += 1;
+        } else{
+            console.log("You win! Rock beats Scissors!");
+            playerScore += 1;
+        }
+    }
+    if (playerChoice === "paper"){
+        if (computerChoice === "paper"){
+            console.log("Draw! Paper vs. Paper");
+        } else if (computerChoice === "scissors"){
+            console.log("Sorry, you lose... Scissors beat Paper");
+            computerScore += 1;
+        } else{
+            console.log("You win! Paper beats Rock!");
+            playerScore += 1;
+        }
+    }
+    if (playerChoice === "scissors"){
+        if (computerChoice === "scissors"){
+            console.log("Draw! Scissors vs. Scissors");
+        } else if (computerChoice === "rock"){
+            console.log("Sorry, you lose... Rosk beats scissors");
+            computerScore += 1;
+        } else{
+            console.log("You win! Scissors beat Paper!");
+            playerScore += 1;
+        }
+    }
+}
+
+if (playerScore > computerScore){
+    console.log(`Congratulations! You win with the score ${playerScore}-${computerScore}`);
+} else if(playerScore < computerScore){
+    console.log(`Sorry... You lose with the score ${computerScore}-${playerScore}`);
+} else{
+    console.log(`Draw! Final score is ${playerScore}-${computerScore}`);
+}
